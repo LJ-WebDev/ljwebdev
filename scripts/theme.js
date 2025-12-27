37166f561f2ab6bfd13f9e1428638d21fa3770fd
LJ WebDev <ljwebdev@proton.me>
2025-12-27 21:30:32 +1100

    Update theme toggle icons for light/dark mode

--- scripts/theme.js
+++ scripts/theme.js
@@ -26,6 +26,20 @@ document.addEventListener('DOMContentLoaded', () => {
   handleTheme();
 
   const themeToggle = document.querySelector('#theme-toggle');
+  const sun = document.querySelector('.sun');
+  const moon = document.querySelector('.moon');
+
+  if (preferLight) {
+    sun.classList.add('swap-off');
+    sun.classList.remove('swap-on');
+    moon.classList.add('swap-on');
+    moon.classList.remove('swap-off');
+  } else {
+    moon.classList.add('swap-off');
+    moon.classList.remove('swap-on');
+    sun.classList.add('swap-on');
+    sun.classList.remove('swap-off');
+  }
 
   if (themeToggle) {
     themeToggle.addEventListener('click', () => {
